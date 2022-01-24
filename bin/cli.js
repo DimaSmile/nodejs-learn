8cd4906ac380d84864078cad696e7b7ed824f576
Dima <efremov2293@gmail.com>
2022-01-24 21:53:24 +0200

    add command to get time by city name

--- bin/cli.js
+++ bin/cli.js
@@ -1,2 +1,24 @@
 #!/usr/bin/env node
 
+import 'dotenv/config'
+
+import got from 'got';
+import { hideBin } from "yargs/helpers";
+
+const url = process.env.ABSTRACTAPI_URL;
+const apiKey = process.env.ABSTRACTAPI_API_KEY;
+const [city] = hideBin(process.argv);
+
+try {
+    const data = await got(url, {
+      searchParams: {
+        location: city,
+        api_key: apiKey
+      }
+    })
+    .json();
+
+    console.log(`${data.timezone_location}: ${data.datetime}`);
+} catch (error) {
+    console.error(`Error: ${error.message}`);
+}
